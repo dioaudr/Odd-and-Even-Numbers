82428afb712d3fb9c12138aae122b0907d64d5fd
Dio Audia <76801729+dioaudr@users.noreply.github.com>
2021-02-09 10:15:39 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+// insert your code here
+//no 1
+for(var num = 1; num <=100; num +=3){
+    if((num % 2)==1){
+        console.log(num + "-" + "ganjil")
+    } else if((num % 2)==0){
+        console.log(num + "-" + "Genap")
+    }
+}
+
+//no 2
+for(var num1 = 50; num1 <=200; num1 +=5){
+    if(num1 % 3 == 0){
+        console.log(num1 + "-" + "Faktor 3")
+    } else {
+        console.log(num1 + "-" + "Tidak bisa dibagi 3")
+ }
+    }
+        
+ //no 3
+ for(var num2 = 100; num2 <=200; num2 +=7){
+    if(num2 % 8 == 0){
+        console.log(num2)
+    }
+}
